191b9a712212b4c45e0f0298b5ffd8d8c970ea6b
huangdian <870003719@qq.com>
2020-07-08 11:29:11 +0800

    1.新增 vue组件精讲练习文件夹

---
+++ vue组件精讲/demo1/src/components/form/form.vue
@@ -0,0 +1,42 @@
+<!-- 组件说明 -->
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+<script>
+//import x from ''
+export default {
+  name: '',
+  components: {
+
+  },
+  props: {
+    prop: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style lang='less' scoped>
+</style>
