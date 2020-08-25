5cda716cc8ab52422606770ce3777db88bbcbae4
huangdian <870003719@qq.com>
2020-08-25 19:48:25 +0800

    1.练习 compositionapi

--- vue3/01-vue3-vite/src/App.vue
+++ vue3/01-vue3-vite/src/App.vue
@@ -5,17 +5,20 @@
   />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
   <todo></todo>
+  <compositionApi></compositionApi>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import todo from './components/todo.vue'
+import compositionApi from './components/compositionApi.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    todo
+    todo,
+    compositionApi
   }
 }
 </script>
