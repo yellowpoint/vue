8493249b42af937e6211d571ab0a9e2e13bdcc69
huangdian <870003719@qq.com>
2020-08-26 15:10:04 +0800

    1.练习 新组件

--- vue3/01-vue3-vite/src/App.vue
+++ vue3/01-vue3-vite/src/App.vue
@@ -6,19 +6,28 @@
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
   <todo></todo>
   <compositionApi></compositionApi>
+  <!-- <newCom></newCom> -->
+  <ul>
+    <newCom :data="[5,3,1,6,9,4,2,8]" />
+  </ul>
+  <newCon_teleport></newCon_teleport>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import todo from './components/todo.vue'
 import compositionApi from './components/compositionApi.vue'
+import newCom from './components/newCom.vue'
+import newCon_teleport from './components/newCon_teleport.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     todo,
-    compositionApi
+    compositionApi,
+    newCom,
+    newCon_teleport
   }
 }
 </script>
